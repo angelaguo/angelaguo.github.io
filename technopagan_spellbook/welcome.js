a54fad4f14713b7b62cbaed8acb0ad8513ceb789
Angela <angela_guo@brown.edu>
2016-05-08 23:43:30 -0400

    added home page

--- technopagan_spellbook/welcome.js
+++ technopagan_spellbook/welcome.js
@@ -1,15 +1,4 @@
 // make UI display foreign home letter
-function showWushuLetter(){
-    $(".puppetry").addClass('hidden');
-    $(".wushu").removeClass('hidden');
-    $("#puppetry").removeClass('active');
-    $('#wushu').addClass('active');
-}
-
-// make UI display single submit form
-function showPuppetLetter(){
-    $(".puppetry").removeClass('hidden');
-    $(".wushu").addClass('hidden');
-    $("#puppetry").addClass('active');
-    $('#wushu').removeClass('active');
+function toggleInstructionVisibility(){
+    $(".help").toggleClass('hidden');
 }
