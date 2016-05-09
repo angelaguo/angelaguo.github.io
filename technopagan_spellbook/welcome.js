9e3b95dfdd6491f0b37aabf1d771923abf0cf5e8
Angela <angela_guo@brown.edu>
2016-05-09 10:59:10 -0400

    final

--- technopagan_spellbook/welcome.js
+++ technopagan_spellbook/welcome.js
@@ -2,3 +2,78 @@
 function toggleInstructionVisibility(){
     $(".help").toggleClass('hidden');
 }
+
+function safety(){
+    $(".safety").removeClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function crystal(){
+    $(".safety").addClass('hidden');
+    $(".crystal").removeClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function truth(){
+    $(".safety").addClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").removeClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function invis(){
+    $(".safety").addClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").removeClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function neutrality(){
+    $(".safety").addClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").removeClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function deformation(){
+    $(".safety").addClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").removeClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").addClass('hidden');
+}
+
+function web(){
+    $(".safety").addClass('hidden');
+    $(".crystal").addClass('hidden');
+    $(".truth").addClass('hidden');
+    $(".neutrality").addClass('hidden');
+    $(".deformation").addClass('hidden');
+    $(".invis").addClass('hidden');
+    $(".web").removeClass('hidden');
+}
+
+function submit(){
+    $(".presubmit").addClass('hidden');
+    $(".postsubmit").removeClass('hidden');
+}
